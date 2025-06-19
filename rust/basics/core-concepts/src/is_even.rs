05b9b1d384d43baa7693940f59252c6fa7ce7e2a
Inesh-Reddy <ineshreddy007@gmail.com>
2025-06-19 15:37:03 +0530

    week-1 : rust basics

---
+++ rust/basics/core-concepts/src/is_even.rs
@@ -0,0 +1,10 @@
+// Write a function is_even that takes a number as an input adn returns true if it is even
+
+pub fn is_even(n: i32) -> &'static str {
+    if n % 2 == 0 {
+        "even"
+    } else {
+        "odd"
+    }
+}
+
---
+++ rust/basics/core-concepts/src/main.rs
@@ -0,0 +1,6 @@
+mod is_even;
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}", is_even::is_even(10));
+}
