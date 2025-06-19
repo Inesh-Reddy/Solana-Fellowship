a1ad3a48c1ff2eeb44692174f4475f576754abcd
Inesh-Reddy <ineshreddy007@gmail.com>
2025-06-19 17:24:50 +0530

    week-1 : rust basics : get string length

--- rust/basics/core-concepts/src/get_string_length.rs
+++ rust/basics/core-concepts/src/get_string_length.rs
@@ -1,9 +1,15 @@
 // Write a function get_String_Length that takes a string as an input and returns its lenght
+fn get_string_lenght_chars(string_toconvert :&str) -> usize {
+    string_toconvert.chars().count()
+}
 
-pub fn get_string_length(input_string: String)-> i32 {
-    let mut count = 0;
-    for _i in 0..input_string.len()  {
-        count = count+1;
-    }
+pub fn get_string_length(input_string: String)-> usize {
+    // let mut count = 0;
+    // for _i in 0..input_string.len()  {
+    //     count = count+1;
+    // }
+    let count = get_string_lenght_chars(& input_string);
     return count;
 }
+
+
