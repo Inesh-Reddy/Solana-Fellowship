55191eb0acc26ce3fedacffb2b6bf6eb551f6aba
Inesh-Reddy <ineshreddy007@gmail.com>
2025-06-19 16:31:47 +0530

    week-1 : rust basics : get string lenght

---
+++ rust/basics/core-concepts/src/get_string_length.rs
@@ -0,0 +1,9 @@
+// Write a function get_String_Length that takes a string as an input and returns its lenght
+
+pub fn get_string_length(input_string: String)-> i32 {
+    let mut count = 0;
+    for _i in 0 .. input_string.len() {
+        count = count+1;
+    }
+    return count;
+}
--- rust/basics/core-concepts/src/main.rs
+++ rust/basics/core-concepts/src/main.rs
@@ -1,8 +1,10 @@
 mod is_even;
 mod fib;
+mod get_string_length;
 
 fn main() {
     println!("Hello, world!");
     println!("{}", is_even::is_even(10));
     println!("{}", fib::fib(4));
+    println!("{}", get_string_length::get_string_length("Rustlings".to_string()));
 }
