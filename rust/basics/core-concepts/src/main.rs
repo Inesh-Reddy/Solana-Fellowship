a99385150461b504dd418a844600ddba794d2f9b
Inesh-Reddy <ineshreddy007@gmail.com>
2025-06-19 16:43:55 +0530

    week-1 : rust basics : get string length

--- rust/basics/core-concepts/src/main.rs
+++ rust/basics/core-concepts/src/main.rs
@@ -6,5 +6,5 @@ fn main() {
     println!("Hello, world!");
     println!("{}", is_even::is_even(10));
     println!("{}", fib::fib(4));
-    println!("{}", get_string_length::get_string_length("Rustlings".to_string()));
+    println!("{}", get_string_length::get_string_length("Rust lings".to_string()));
 }
