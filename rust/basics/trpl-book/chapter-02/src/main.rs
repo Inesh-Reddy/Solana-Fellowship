472bd38bf290aed331914f4e49b2c82d5c29ea1e
Inesh-Reddy <ineshreddy007@gmail.com>
2025-06-20 16:33:10 +0530

    Rust Book : Chapter-02 : guessing game

--- rust/basics/trpl-book/chapter-02/src/main.rs
+++ rust/basics/trpl-book/chapter-02/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world! chapter 2 ");
+    println!("The Rust Programming language Book : chapter 2 ");
+
+    println!("Guess the number!");
+    println!("Please input your guess : ");
+
+    let mut guess = String::from("");
+
+    std::io::stdin()
+        .read_line( &mut guess)
+        .expect("you got some error");
+
+    println!("You guessed: {guess}");
+
 }
