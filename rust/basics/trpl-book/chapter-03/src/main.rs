dc0120acc13ea2238e748ab39f6f8be6b93f3a9a
Inesh-Reddy <ineshreddy007@gmail.com>
2025-06-23 12:50:57 +0530

    shadowing, mutability, referencing

--- rust/basics/trpl-book/chapter-03/src/main.rs
+++ rust/basics/trpl-book/chapter-03/src/main.rs
@@ -1,8 +1,17 @@
-use crate::variablesmutability::variables;
+use crate::variablesmutability::{mutability, reference, shadowing, variables};
 
 mod variablesmutability;
 
 fn main() {
-    variables();
+
+    
     println!("Hello, world!");
+    println!("------- variabels -------");
+    variables();
+    println!("------- shadowing -------");
+    shadowing();
+    println!("------- mutability -------");
+    mutability();
+    println!("------- referencing -------");
+    reference();
 }
--- rust/basics/trpl-book/chapter-03/src/variablesmutability.rs
+++ rust/basics/trpl-book/chapter-03/src/variablesmutability.rs
@@ -14,3 +14,51 @@ pub fn variables() {
     println!("{THREE_HOURS_IN_SECONDS}");
     // println!("{}", THREE_HOURS_IN_SECONDS);
 }
+
+pub fn shadowing( ) {
+    let x = 5;
+    let x = x+1;
+    {
+        let x = x* 2;
+        println!("{x}");
+    }
+    println!("{x}");
+    // easy to change types : 
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("spaces: {spaces}");
+
+
+
+    //tricky one
+    let mut x: u32 = 1;
+    {
+        let mut x = x;
+        x += 2;
+        println!("trickyone1: {x}");
+    }
+    println!("tricyone2:{x}");
+
+}
+
+pub fn mutability(){
+    let mut x = 7;
+    println!("{x}");
+    x = 10;
+    println!("{x}");
+}
+
+pub fn reference(){
+    let mut x = 5;
+    {
+
+        let y = & x;
+        println!("{y}");
+        let y = &mut x;
+        *y = 1;
+        println!("{y}");
+        let y = &*y+1;
+        println!("{y}");
+    }
+    println!("{x}");
+}
