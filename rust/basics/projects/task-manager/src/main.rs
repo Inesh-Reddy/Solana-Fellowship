1aa511d2a09296757923e6a8da397eb0478bcfa8
Inesh-Reddy <ineshreddy007@gmail.com>
2025-07-04 11:08:54 +0530

    completed modifying and deleting handlers

--- rust/basics/projects/task-manager/src/main.rs
+++ rust/basics/projects/task-manager/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Write};
 
-use crate::task_handlers::{adding, viewing};
+use crate::task_handlers::{adding, completing, removing_tasks, viewing};
 mod task_handlers;
 mod state;
 fn main() {
@@ -35,12 +35,30 @@ fn main() {
             }else if input=="2" {
                 let result = viewing();
                 for i in 0..result.len() {
-                    println!("✅ Task {}: {} ", i+1,result[i]);
+                    println!("Task {}: {} ", i+1,result[i]);
                 }
             }else if input=="3" {
-                
+                let result = viewing();
+                for i in 0..result.len() {
+                    println!("Task {}: {} ", i+1,result[i]);
+                }
+                println!("Please input todo number to modify:");
+                let mut todo_no_to_modify = String::new();
+                std::io::stdin().read_line(&mut todo_no_to_modify).expect("please provide proper number");
+                let item_to_modify = todo_no_to_modify.trim().parse::<i32>().unwrap();
+                let result = completing(item_to_modify as usize);
+                println!("{result}");
             }else if input=="4" {
-                
+                let result = viewing();
+                for i in 0..result.len() {
+                    println!("Task {}: {} ", i+1,result[i]);
+                }
+                println!("Please input todo number to DELETE:");
+                let mut todo_no_to_delete = String::new();
+                std::io::stdin().read_line(&mut todo_no_to_delete).expect("please provide proper number");
+                let item_to_delete = todo_no_to_delete.trim().parse::<i32>().unwrap();
+                let result = removing_tasks(item_to_delete as usize);
+                println!("{result}");
             }else if input=="5" {
                 println!("Thank you please visit again!!");
                 break 'outer_loop ;
--- rust/basics/projects/task-manager/src/task_handlers.rs
+++ rust/basics/projects/task-manager/src/task_handlers.rs
@@ -1,24 +1,38 @@
-use crate::state::TASKS;
+use crate::{state::TASKS};
 
 pub fn adding(task_to_add: String) -> String {
     println!("🧪 adding task .....");
     let cleaned = task_to_add.trim().to_string(); 
     let mut tasks = TASKS.lock().unwrap();
-    tasks.push(cleaned.clone());
+    tasks.push(format!("{} : ❌",cleaned.clone()));
     let to_return = format!("✅ Task: {} added successfully", cleaned);
     to_return
 }
 
 pub fn viewing() -> Vec<String> {
-    println!("💭 Fetching tasks....");
-    let mut tasks = TASKS.lock().unwrap();
+    println!("💭 ");
+    let tasks = TASKS.lock().unwrap();
     tasks.clone()
 }
 
-pub fn completing() {
-
+pub fn completing(todo_to_modify:usize)-> String {
+    println!("called modify function");
+    let mut task = TASKS.lock().unwrap();
+    if (todo_to_modify as usize) >= task.len() {
+        return "❌ Please provide a proper todo number.".to_string();
+    }else {
+        task[todo_to_modify as usize] = format!("{} : ✅ ", task[todo_to_modify as usize]);
+        return "✅ task modified".to_string();
+    }
 }
 
-pub fn removing_tasks() {
-
+pub fn removing_tasks(todo_to_delete:usize) -> String {
+    println!("called DELETE function");
+    let mut task = TASKS.lock().unwrap();
+    if (todo_to_delete as usize) >= task.len() {
+        return "❌ Please provide a proper todo number to DELETE.".to_string();
+    }else {
+        task.remove(todo_to_delete);
+        return "✅ task DELETED".to_string();
+    }
 }
